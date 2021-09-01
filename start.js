1efd065a107372b6429b39538f577463263d38b1
Anon Muangwijit <anonmuangwijit@gmail.com>
2021-09-01 17:03:04 +0700

    create function onMouseMove

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 (() => {
   const bodyElem = document.querySelector("body");
   const eyeElems = document.querySelectorAll(".eye");
+  function onMouseMove({ pageX, pageY }) {
+  }
   function run() {
   }
 
